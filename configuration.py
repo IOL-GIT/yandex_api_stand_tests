e029644198e90e125fc26afe285eaba44cae6b00
Олег Ильчук <iol@MacBook-Air-Oleg.local>
2023-07-05 22:41:38 +0500

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = 'https://9decc0ad-592e-4c8c-8a0f-d3b93ac875d5.serverhub.praktikum-services.ru'
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
